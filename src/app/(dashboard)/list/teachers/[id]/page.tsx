e19ceb7844b8bfb737550019b07ab7fd05e9605d
unknown <Opierah@Opsydev.(none)>
2026-07-19 10:18:17 +0100

    Teacher Page Added

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -113,11 +113,21 @@ const SingleTeacherPage = () => {
                 <div className="bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Shortcuts</h1>
                     <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-                        <Link className="p-3 rounded-md bg-lopeSkyLight" href="/">Teacher&apos;s Classes</Link>
-                        <Link className="p-3 rounded-md bg-lopeEmeraldLight" href="/">Teacher&apos;s Students</Link>
-                        <Link className="p-3 rounded-md bg-lopeAmberLight" href="/">Teacher&apos;s Lessons</Link>
-                        <Link className="p-3 rounded-md bg-lopeRoseLight" href="/">Teacher&apos;s Exams</Link>
-                        <Link className="p-3 rounded-md bg-lopeIndigoLight" href="/">Teacher&apos;s Assignment</Link>
+                        <Link className="p-3 rounded-md bg-lopeSkyLight" href="/">
+                            Teacher&apos;s Classes
+                        </Link>
+                        <Link className="p-3 rounded-md bg-lopeEmeraldLight" href={`/list/students?teacherId=${"teacher2"}`}>
+                            Teacher&apos;s Students
+                        </Link>
+                        <Link className="p-3 rounded-md bg-lopeAmberLight" href="/">
+                            Teacher&apos;s Lessons
+                        </Link>
+                        <Link className="p-3 rounded-md bg-lopeRoseLight" href="/">
+                            Teacher&apos;s Exams
+                        </Link>
+                        <Link className="p-3 rounded-md bg-lopeIndigoLight" href="/">
+                            Teacher&apos;s Assignment
+                        </Link>
                     </div>
                 </div>
                 <Performance />
