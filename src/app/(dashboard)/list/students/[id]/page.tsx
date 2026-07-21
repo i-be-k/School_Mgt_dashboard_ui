defaf786f93c87d3d40ff713e41bcfb7c5c56e12
unknown <Opierah@Opsydev.(none)>
2026-07-21 23:54:54 +0100

    Single Student Page Added

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -92,11 +92,21 @@ const SingleStudentPage = () => {
                 <div className="bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Shortcuts</h1>
                     <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-                        <Link className="p-3 rounded-md bg-lopeSkyLight" href="/">Student&apos;s Lessons</Link>
-                        <Link className="p-3 rounded-md bg-lopeEmeraldLight" href="/">Student&apos;s Teachers</Link>
-                        <Link className="p-3 rounded-md bg-lopeRoseLight" href="/">Student&apos;s Exams</Link>
-                        <Link className="p-3 rounded-md bg-lopeIndigoLight" href="/">Student&apos;s Assignments</Link>
-                        <Link className="p-3 rounded-md bg-lopeAmberLight" href="/">Student&apos;s Results</Link>
+                        <Link className="p-3 rounded-md bg-lopeSkyLight" href={`/list/lessons?classId=${2}`}>
+                            Student&apos;s Lessons
+                        </Link>
+                        <Link className="p-3 rounded-md bg-lopeEmeraldLight" href={`/list/teachers?classId=${2}`}>
+                            Student&apos;s Teachers
+                        </Link>
+                        <Link className="p-3 rounded-md bg-lopeRoseLight" href={`/list/exams?classId=${2}`}>
+                            Student&apos;s Exams
+                        </Link>
+                        <Link className="p-3 rounded-md bg-lopeIndigoLight" href={`/list/assignments?classId=${2}`}>
+                            Student&apos;s Assignments
+                        </Link>
+                        <Link className="p-3 rounded-md bg-lopeAmberLight" href={`/list/results?studentId=${"student2"}`}>
+                            Student&apos;s Results
+                        </Link>
                     </div>
                 </div>
                 <Performance />
