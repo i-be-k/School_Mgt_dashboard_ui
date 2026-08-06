810c5e96d88d4a0af6b1403389bd2119b0c5da4d
okobitech <ibkodify@gmail.com>
2026-08-06 08:04:25 +0100

    Modified Menu Component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -128,25 +128,23 @@ const Menu = async () => {
             {i.title}
           </span>
           {i.items.map((item) => {
-            if(item.visible.includes(role)){
+            if (item.visible.includes(role)) {
               return (
-                (
-                  <Link
-                    href={item.href}
-                    key={item.label}
-                    className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lopeRoseLight"
-                  >
-                    <Image src={item.icon} alt="" width={20} height={20} />
-                    <span className="hidden lg:block">{item.label}</span>
-                  </Link>
-                )
-              )
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lopeRoseLight"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
             }
-          } )}
+          })}
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default Menu;
