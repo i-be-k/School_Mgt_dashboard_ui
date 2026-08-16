088e3c9a1b685455f23df3dd8538998a8f6fa76a
okobitech <ibkodify@gmail.com>
2026-08-16 23:39:25 +0100

    Modified Teachers Page

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,11 +1,46 @@
 import Announcements from "@/components/Announcements";
-import BigCalendar from "@/components/BigCalendar";
-import FormModal from "@/components/FormModal";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
+import FormContainer from "@/components/FormContainer";
 import Performance from "@/components/Performance";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { Teacher } from "@/generated/prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
-const SingleTeacherPage = () => {
+interface PageProps {
+    params: Promise<{ id: string }>;
+}
+
+const SingleTeacherPage = async ({
+    params
+}: PageProps) => {
+    const { id } = await params;
+
+    const { sessionClaims } = await auth();
+    const role = (sessionClaims?.metadata as { role?: string })?.role;
+
+    const teacher:
+        | (Teacher & {
+            _count: { subjects: number; lessons: number; classes: number };
+        })
+        | null = await prisma.teacher.findUnique({
+            where: { id },
+            include: {
+                _count: {
+                    select: {
+                        subjects: true,
+                        lessons: true,
+                        classes: true,
+                    },
+                },
+            },
+        });
+
+    if (!teacher) {
+        return notFound();
+    }
     return (
         <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
             {/* LEFT */}
@@ -16,52 +51,42 @@ const SingleTeacherPage = () => {
                     <div className="bg-lopeSky py-6 px-4 rounded-md flex-1 flex gap-4">
                         <div className="w-1/3">
                             <Image
-                                src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                                src={teacher.img || "/noAvatar.png"}
                                 alt=""
                                 width={144}
                                 height={144}
-                                className="w-36 h-36 rounded-full object-cover"
+                                className="w-35 h-35 rounded-full object-cover"
                             />
                         </div>
                         <div className="w-2/3 flex flex-col justify-between gap-4">
                             <div className="flex items-center gap-4">
-                                <h1 className="text-xl font-semibold">Leonard Synard</h1>
-                                <FormModal
-                                    table="teacher"
-                                    type="update"
-                                    data={{
-                                        id: 1,
-                                        username: "deanguerrero",
-                                        email: "deanguerrero@gmail.com",
-                                        password: "password",
-                                        firstName: "Dean",
-                                        lastName: "Guerrero",
-                                        phone: "+234 123 456 7890",
-                                        address: "1234 Main St, Anytown, USA",
-                                        bloodType: "A+",
-                                        dateOfBirth: "2000-01-01",
-                                        sex: "male",
-                                        img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200",
-                                    }}
-                                />
+                                <h1 className="text-xl font-semibold">
+                                    {teacher.name + " " + teacher.surname}
+                                </h1>
+                                {role === "admin" && (
+                                    <FormContainer
+                                        table="teacher"
+                                        type="update"
+                                        data={teacher}
+                                    />)}
                             </div>
                             <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                             <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                     <Image src="/blood.png" alt="" width={14} height={14} />
-                                    <span>A+</span>
+                                    <span>{teacher.bloodType}</span>
                                 </div>
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                     <Image src="/date.png" alt="" width={14} height={14} />
-                                    <span>January 2027</span>
+                                    <span>{new Intl.DateTimeFormat("en-UK").format(teacher.birthday)}</span>
                                 </div>
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                     <Image src="/mail.png" alt="" width={14} height={14} />
-                                    <span>user@gmail.com</span>
+                                    <span>{teacher.email || "-"}</span>
                                 </div>
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                     <Image src="/phone.png" alt="" width={14} height={14} />
-                                    <span>+234 123 456 7890</span>
+                                    <span>{teacher.phone || "-"}</span>
                                 </div>
                             </div>
                         </div>
@@ -80,15 +105,15 @@ const SingleTeacherPage = () => {
                         <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
                             <Image src="/singleBranch.png" alt="" width={24} height={24} className="w-6 h-6" />
                             <div className="">
-                                <h1 className="text-xl font-semibold">2</h1>
-                                <span className="text-sm text-gray-400">Branch</span>
+                                <h1 className="text-xl font-semibold">{teacher._count.subjects}</h1>
+                                <span className="text-sm text-gray-400">Subject</span>
                             </div>
                         </div>
                         {/* CARD */}
                         <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
                             <Image src="/singleLesson.png" alt="" width={24} height={24} className="w-6 h-6" />
                             <div className="">
-                                <h1 className="text-xl font-semibold">6</h1>
+                                <h1 className="text-xl font-semibold">{teacher._count.lessons}</h1>
                                 <span className="text-sm text-gray-400">Lessons</span>
                             </div>
                         </div>
@@ -96,7 +121,7 @@ const SingleTeacherPage = () => {
                         <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
                             <Image src="/singleClass.png" alt="" width={24} height={24} className="w-6 h-6" />
                             <div className="">
-                                <h1 className="text-xl font-semibold">6</h1>
+                                <h1 className="text-xl font-semibold">{teacher._count.classes}</h1>
                                 <span className="text-sm text-gray-400">Classes</span>
                             </div>
                         </div>
@@ -105,11 +130,11 @@ const SingleTeacherPage = () => {
                 {/* BOTTOM */}
                 <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
                     <h1>Teacher&apos;s Schedule</h1>
-                    <BigCalendar />
+                    <BigCalendarContainer type="teacherId" id={teacher.id} />
                 </div>
             </div>
             {/* RIGHT */}
-            <div className="w-full xl:w-1/3 flex flex-col gap">
+            <div className="w-full xl:w-1/3 flex flex-col gap-4">
                 <div className="bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Shortcuts</h1>
                     <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
