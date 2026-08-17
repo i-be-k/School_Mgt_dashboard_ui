a8ab1015428471655b037bf7c8a29fb2a84b056c
okobitech <ibkodify@gmail.com>
2026-08-17 21:13:29 +0100

    Modified Teachers Page

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,80 +1,99 @@
-import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { Prisma } from "@/generated/prisma/client";
-import { role, teachersData } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
 import { Class, Subject, Teacher } from "@/generated/prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import FormContainer from "@/components/FormContainer";
+import { auth } from "@clerk/nextjs/server";
 
-type TeacherList = Teacher & { subjects: Subject[] } & { classes: Class[] }
 
-const columns = [
-    {
-        header: "Info", accessor: "info"
-    },
-    {
-        header: "Teacher ID", accessor: "teacherId", className: "hidden md:table-cell",
-    },
-    {
-        header: "Subjects", accessor: "subject", className: "hidden md:table-cell",
-    },
-    {
-        header: "Classes", accessor: "classes", className: "hidden md:table-cell",
-    },
-    {
-        header: "Phone", accessor: "phoneId", className: "hidden md:table-cell",
-    },
-    {
-        header: "Address", accessor: "addressId", className: "hidden md:table-cell",
-    },
-    {
-        header: "Action", accessor: "action",
-    },
-];
+type TeacherList = Teacher & { subjects: Subject[] } & { classes: Class[] };
 
-const renderRow = (item: TeacherList) => (
-    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lopeRoseLight">
-        <td className="flex items-center gap-4 p-4">
-            <Image src={item.img || "/noAvatar.png"} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover" />
-            <div className="flex flex-col">
-                <h3 className="font-semibold">{item.name}</h3>
-                <p className="text-xs text-gray-500">{item?.email}</p>
-            </div>
-        </td>
-        <td className="hidden md:table-cell">{item.username}</td>
-        <td className="hidden md:table-cell">{item.subjects.map(subject => subject.name).join(", ")}</td>
-        <td className="hidden md:table-cell">{item.classes.map(classItem => classItem.name).join(", ")}</td>
-        <td className="hidden md:table-cell">{item.phone}</td>
-        <td className="hidden md:table-cell">{item.address}</td>
-        <td>
-            <div className="flex items-center gap-2">
-                <Link href={`/list/teachers/${item.id}`}>
-                    <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lopeSky" title="viewbtn">
-                        <Image src="/view.png" alt="" width={16} height={16} />
-                    </button>
-                </Link>
-                {role === "admin" && (
-                    // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lopeIndigo" title="deletebtn">
-                    //     <Image src="/delete.png" alt="" width={16} height={16} />
-                    // </button>
-                    <FormModal table="teacher" type="delete" id={item.id} />
-                )}
-            </div>
-        </td>
-    </tr>
-);
 
 const TeacherListPage = async ({
     searchParams,
 }: {
     searchParams: { [key: string]: string | undefined };
 }) => {
+    const { userId, sessionClaims } = await auth();
+    const role = (sessionClaims?.metadata as { role?: string })?.role;
+    const columns = [
+        {
+            header: "Info",
+            accessor: "info"
+        },
+        {
+            header: "Teacher ID",
+            accessor: "teacherId",
+            className: "hidden md:table-cell",
+        },
+        {
+            header: "Subjects",
+            accessor: "subject",
+            className: "hidden md:table-cell",
+        },
+        {
+            header: "Classes",
+            accessor: "classes",
+            className: "hidden md:table-cell",
+        },
+        {
+            header: "Phone",
+            accessor: "phoneId",
+            className: "hidden md:table-cell",
+        },
+        {
+            header: "Address",
+            accessor: "addressId",
+            className: "hidden md:table-cell",
+        },
+        ...(role === "admin"
+            ? [
+                {
+                    header: "Actions",
+                    accessor: "action",
+                },
+            ]
+            : []),
+    ];
 
-    const { page, ...queryParams } = searchParams;
+    const renderRow = (item: TeacherList) => (
+        <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lopeRoseLight">
+            <td className="flex items-center gap-4 p-4">
+                <Image src={item.img || "/noAvatar.png"} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover" />
+                <div className="flex flex-col">
+                    <h3 className="font-semibold">{item.name}</h3>
+                    <p className="text-xs text-gray-500">{item?.email}</p>
+                </div>
+            </td>
+            <td className="hidden md:table-cell">{item.username}</td>
+            <td className="hidden md:table-cell">{item.subjects.map(subject => subject.name).join(", ")}</td>
+            <td className="hidden md:table-cell">{item.classes.map(classItem => classItem.name).join(", ")}</td>
+            <td className="hidden md:table-cell">{item.phone}</td>
+            <td className="hidden md:table-cell">{item.address}</td>
+            <td>
+                <div className="flex items-center gap-2">
+                    <Link href={`/list/teachers/${item.id}`}>
+                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lopeSky" title="viewBtn">
+                            <Image src="/view.png" alt="" width={16} height={16} />
+                        </button>
+                    </Link>
+                    {role === "admin" && (
+                        // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lopeIndigo" title="deleteBtn">
+                        //     <Image src="/delete.png" alt="" width={16} height={16} />
+                        // </button>
+                        <FormContainer table="teacher" type="delete" id={item.id} />
+                    )}
+                </div>
+            </td>
+        </tr>
+    );
+
+    const { page, ...queryParams } = await searchParams;
 
     const p = page ? parseInt(page) : 1;
 
@@ -134,7 +153,7 @@ const TeacherListPage = async ({
                             // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lopeAmber" title="addTeacher">
                             //     <Image src="/plus.png" alt="" width={14} height={14} />
                             // </button>
-                            <FormModal table="teacher" type="create" />
+                            <FormContainer table="teacher" type="create" />
                         )}
                     </div>
                 </div>
