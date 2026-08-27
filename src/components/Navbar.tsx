d07e4508bcb49d6cf5e776c0fd97a7b15a99d39e
okobitech <ibkodify@gmail.com>
2026-08-27 16:04:49 +0100

    Fixed Harded User Name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = async () => {
                     </div>
                 </div>
                 <div className="flex flex-col">
-                    <span className="text-sm leading-3 font-medium">Laju Obi</span>
+                    <span className="text-sm leading-3 font-medium">{user?.firstName} {user?.lastName}</span>
                     <span className="text-[10px] text-gray-500 text-right">{user?.publicMetadata?.role as string}</span>
                 </div>
                 {/* <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full"/> */}
